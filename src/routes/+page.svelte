85bccf64193d027a6c564bab873dc05e0608a3ca
Shrv Bldna <151522619+ashrv12@users.noreply.github.com>
2025-08-19 20:23:14 +0800

    Fix svg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <main class="relative flex h-dvh w-dvw items-center justify-center">
 	<img
 		class="absolute top-1/2 left-1/2 z-1 h-[100px] w-[100px] -translate-x-1/2 -translate-y-1/2"
-		src="/earth.svg"
+		src="./earth.svg"
 		alt="earth"
 	/>
 	<Dashboard />
