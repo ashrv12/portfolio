27a116ebf698f2e80b350e641df507d9d2ca7756
Shrv Bldna <151522619+ashrv12@users.noreply.github.com>
2025-08-19 21:23:34 +0800

    Finished two of 5

--- src/lib/components/dashboard-components/Experience.svelte
+++ src/lib/components/dashboard-components/Experience.svelte
@@ -1,3 +1,36 @@
-<div class="col-start-1 col-end-3 row-start-3 row-end-6 border">
-	<h1>Experience</h1>
+<div
+	class="col-start-1 col-end-3 row-start-3 row-end-6 flex flex-col justify-between rounded-sm bg-[#99E8CE] p-2 text-[var(--primary-bg-color)]"
+>
+	<h1 class="text-4xl font-bold">Work Experience</h1>
+	<div class="flex flex-col gap-2">
+		<div>
+			<h2 class="jetbrains-mono border-b-[1px] text-sm font-bold md:text-lg">
+				DevSecOps Engineer <span class="font-black text-[#C60F7B]">[Trade Development Bank]</span>
+			</h2>
+			<p class="text-xs">
+				Worked on bare metal Kubernetes and Docker deployment environments building pipelines and
+				fullstack applications.
+			</p>
+		</div>
+
+		<div>
+			<h2 class="jetbrains-mono border-b-[1px] text-sm font-bold md:text-lg">
+				Software Engineer <span class="font-black text-[#C60F7B]">[Pinecone Academy]</span>
+			</h2>
+			<p class="text-xs">
+				Built student teacher CMS based learning platform and lead multiple teams of interns as head
+				project lead whilst also participating in development.
+			</p>
+		</div>
+
+		<div>
+			<h2 class="jetbrains-mono border-b-[1px] text-sm font-bold md:text-lg">
+				Frontend Developer <span class="font-black text-[#C60F7B]">[START]</span>
+			</h2>
+			<p class="text-xs">
+				Worked as a frontend developer on multiple websites and studied Graphics design from a
+				professional Graffiti artist.
+			</p>
+		</div>
+	</div>
 </div>
--- src/lib/components/dashboard-components/Title.svelte
+++ src/lib/components/dashboard-components/Title.svelte
@@ -1,3 +1,15 @@
-<div class="col-start-1 col-end-4 row-start-1 row-end-3 rounded-sm border border-[#e9ebdf]/25 p-2">
-	<h1>DevSecOps Engineer</h1>
+<div
+	class="col-start-1 col-end-4 row-start-1 row-end-3 flex flex-col justify-between rounded-sm bg-[#c3dedd] p-2 text-(--primary-bg-color)"
+>
+	<h1 class="text-4xl font-bold">DevSecOps Engineer</h1>
+
+	<div class="">
+		<h2 class="text-xl font-thin">
+			Ari<i class="bg-(--primary-bg-color)">privacy</i>.B
+		</h2>
+		<p class="jetbrains-mono text-justify md:text-xl">
+			🇨🇭Swiss army knife <i class="font-bold">developer</i> with a DevSecOps specialty. Fluent in
+			English and 🦀 <i class="font-bold">rust</i>.
+		</p>
+	</div>
 </div>
