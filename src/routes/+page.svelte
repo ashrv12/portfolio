ed7a1450bac1eed9675f0d893ec5ac5241072f42
Shrv Bldna <151522619+ashrv12@users.noreply.github.com>
2025-08-19 19:30:33 +0800

    Added my svg to static

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import earth from '$lib/assets/earth.svg';
 	import Dashboard from '$lib/components/Dashboard.svelte';
 </script>
 
 <main class="relative flex h-dvh w-dvw items-center justify-center">
 	<img
 		class="absolute top-1/2 left-1/2 z-1 h-[100px] w-[100px] -translate-x-1/2 -translate-y-1/2"
-		src={earth}
+		src="/earth.svg"
 		alt="earth"
 	/>
 	<Dashboard />
