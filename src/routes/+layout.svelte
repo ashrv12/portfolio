b13b7821e2750388b11173071b100ad9e4df1ba7
Shrv Bldna <151522619+ashrv12@users.noreply.github.com>
2025-08-19 19:40:04 +0800

    Update svelte config js

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/bocchi.jpeg';
 
 	let { children } = $props();
 </script>
