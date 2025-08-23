aa1ed6dea432f061869fcc86b4394647b16051c1
Shrv Bldna <151522619+ashrv12@users.noreply.github.com>
2025-08-23 15:02:11 +0800

    MAKE IT SMALLLERRRRR

--- src/lib/components/dashboard-components/LangnLoc.svelte
+++ src/lib/components/dashboard-components/LangnLoc.svelte
@@ -2,7 +2,7 @@
 	class="col-start-4 col-end-6 row-start-1 row-end-3 flex flex-col justify-between rounded-sm bg-[#575799] p-2"
 >
 	<h1 class="text-xl font-bold sm:text-4xl">Languages & Location</h1>
-	<div class="jetbrains-mono text-sm md:text-2xl">
+	<div class="jetbrains-mono text-xs sm:text-sm md:text-2xl">
 		<h2 class="">🇺🇸 <u>English</u> <span class="font-black">[Fluent]</span></h2>
 		<h2 class="">🇩🇪 <u>German</u> <span class="font-black">[B2]</span></h2>
 		<h2 class="">🇲🇳 <u>Mongolian</u> <span class="font-black">[Fluent]</span></h2>
