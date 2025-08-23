db7c393050767ff3849e2fac1e9d2e238644a0fb
Shrv Bldna <151522619+ashrv12@users.noreply.github.com>
2025-08-23 13:53:36 +0800

    Added kirby

--- src/lib/components/dashboard-components/Skills.svelte
+++ src/lib/components/dashboard-components/Skills.svelte
@@ -1,3 +1,32 @@
-<div class="col-start-3 col-end-6 row-start-4 row-end-6 rounded-sm bg-[#61212D] p-2">
+<div
+	class="col-start-3 col-end-6 row-start-4 row-end-6 flex flex-col justify-between rounded-sm bg-[#61212D] p-2"
+>
 	<h1 class="text-4xl font-bold">Skills</h1>
+
+	<div class="jetbrains-mono flex flex-col gap-3">
+		<div class="flex items-center justify-between rounded-md border-t-[1px] border-b-[1px] px-3">
+			<h2>rust programming language</h2>
+			<div class="flex w-[10%] items-center justify-center">
+				<img src="rust.png" class="h-[2rem] object-scale-down" alt="rust" />
+			</div>
+		</div>
+		<div class="flex items-center justify-between rounded-md border-t-[1px] border-b-[1px] px-3">
+			<h2>docker</h2>
+			<div class="flex w-[10%] items-center justify-center">
+				<img src="docker.svg" class="h-[2rem]" alt="docker" />
+			</div>
+		</div>
+		<div class="flex items-center justify-between rounded-md border-t-[1px] border-b-[1px] px-3">
+			<h2>kubernetes</h2>
+			<div class="flex w-[10%] items-center justify-center">
+				<img src="k8s.png" class="h-[2rem]" alt="k8s" />
+			</div>
+		</div>
+		<div class="flex items-center justify-between rounded-md border-t-[1px] border-b-[1px] px-3">
+			<h2>go programming language</h2>
+			<div class="flex w-[10%] items-center justify-center">
+				<img src="gopher.svg" class="h-[2rem]" alt="go" />
+			</div>
+		</div>
+	</div>
 </div>
